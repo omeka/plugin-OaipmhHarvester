c134c5f864858fb0add7ef20cd34e4e8687367d2
Jim Safley <jimsafley@gmail.com>
2009-04-20 16:02:10 +0000

    Adds inputs divs in config_form.php

--- config_form.php
+++ config_form.php
@@ -10,32 +10,37 @@
 ?>
 <div class="field">
     <label for="oaipmh_harvester_php_path">Path to PHP-CLI</label>
-    <?php echo __v()->formText('oaipmh_harvester_php_path', $path, null);?>
-    <p class="explanation">Path to your server's PHP-CLI command. The PHP 
-    version must correspond to normal Omeka requirements. Some web hosts use PHP 
-    4.x for their default PHP-CLI, but many provide an alternative path to a 
-    PHP-CLI 5 binary. Check with your web host for more information.</p>
+    <div class="inputs">
+        <?php echo __v()->formText('oaipmh_harvester_php_path', $path, null);?>
+        <p class="explanation">Path to your server's PHP-CLI command. The PHP 
+        version must correspond to normal Omeka requirements. Some web hosts use PHP 
+        4.x for their default PHP-CLI, but many provide an alternative path to a 
+        PHP-CLI 5 binary. Check with your web host for more information.</p>
+    </div>
 </div>
 <div class="field">
     <label for="oaipmh_harvester_memory_limit">Memory Limit</label>
-    <?php echo __v()->formText('oaipmh_harvester_memory_limit', $memoryLimit, null);?>
-    <p class="explanation">Set a memory limit to avoid memory allocation errors 
-    during harvesting. <strong>We recommend that you choose a high memory limit.</strong> 
-    Examples include 128M, 1G, and -1. The available options are K (for 
-    Kilobytes), M (for Megabytes) and G (for Gigabytes). Anything else assumes 
-    bytes. Set to -1 for an infinite limit. Be advised that many web hosts set a 
-    maximum memory limit, so this setting may be ignored if it exceeds the 
-    maximum allowable limit. Check with your web host for more information.</p>
+    <div class="inputs">
+        <?php echo __v()->formText('oaipmh_harvester_memory_limit', $memoryLimit, null);?>
+        <p class="explanation">Set a memory limit to avoid memory allocation errors 
+        during harvesting. <strong>We recommend that you choose a high memory limit.</strong> 
+        Examples include 128M, 1G, and -1. The available options are K (for 
+        Kilobytes), M (for Megabytes) and G (for Gigabytes). Anything else assumes 
+        bytes. Set to -1 for an infinite limit. Be advised that many web hosts set a 
+        maximum memory limit, so this setting may be ignored if it exceeds the 
+        maximum allowable limit. Check with your web host for more information.</p>
+    </div>
 </div>
 <div class="field">
     <label for="oaipmh_harvester_ignore_deleted_records">Ignore Deleted Records</label>
-    <?php echo __v()->formCheckbox('oaipmh_harvester_ignore_deleted_records', 
-                                   $ignoreDeletedRecords, 
-                                   null, 
-                                   array('yes', 'no')); ?>
-    <div style="clear:both" />
-    <p class="explanation">Sometimes OAI-PMH data providers will mark records as 
-    delted if they were deleted from their repository. If this is checked the 
-    harvest process will ignore deleted records. If this is unchecked, all such 
-    records will be inserted into Omeka as empty items.</p>
+    <div class="inputs">
+        <?php echo __v()->formCheckbox('oaipmh_harvester_ignore_deleted_records', 
+                                       $ignoreDeletedRecords, 
+                                       null, 
+                                       array('yes', 'no')); ?>
+        <p class="explanation">Sometimes OAI-PMH data providers will mark records as 
+        delted if they were deleted from their repository. If this is checked the 
+        harvest process will ignore deleted records. If this is unchecked, all such 
+        records will be inserted into Omeka as empty items.</p>
+    </div>
 </div>

efd35dda639b32ba7c065a73f5ee6d71398009e9
Kristopher Kelly <kristopherbkelly@gmail.com>
2011-08-07 18:19:38 -0700

    Do not continue harvesting deleted harvests.

--- models/OaipmhHarvester/Job.php
+++ models/OaipmhHarvester/Job.php
@@ -20,6 +20,14 @@ class OaipmhHarvester_Job extends Omeka_JobAbstract
                 "Harvest with id = '$this->_harvestId' does not exist.");
         }
 
+        // Resent jobs can remain queued after all the items themselves have 
+        // been deleted. Skip if that's the case.
+        if ($harvest->status == OaipmhHarvester_Harvest::STATUS_DELETED) {
+            _log("Queued harvest with ID = {$harvest->id} was deleted prior "
+               . "to running this job.");
+            return;
+        }
+
         require_once 'OaipmhHarvester/Harvest/Abstract.php';
         $harvester = OaipmhHarvester_Harvest_Abstract::factory($harvest);
         $harvester->harvest();

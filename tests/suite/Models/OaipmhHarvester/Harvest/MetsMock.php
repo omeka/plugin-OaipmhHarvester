9ee6ca6e3b4925ee22deb4a6ef12fbc0502297a8
Daniel Berthereau <Daniel.github@Berthereau.net>
2015-06-01 00:00:00 +0200

    Forced reorder of files of an item when some files are deleted.

--- tests/suite/Models/OaipmhHarvester/Harvest/MetsMock.php
+++ tests/suite/Models/OaipmhHarvester/Harvest/MetsMock.php
@@ -14,16 +14,18 @@ class OaipmhHarvester_Harvest_MetsMock extends OaipmhHarvester_Harvest_Mets
     {
         $harvestedRecord = parent::_harvestRecord($record);
 
-        // Define the transfer type as local file system.
-        $harvestedRecord['fileMetadata'][Builder_Item::FILE_TRANSFER_TYPE] = 'Filesystem';
-        $harvestedRecord['fileMetadata'][Builder_Item::FILE_INGEST_OPTIONS] = array();
+        if (!empty($harvestedRecord['fileMetadata']['files'])) {
+            // Define the transfer type as local file system.
+            $harvestedRecord['fileMetadata'][Builder_Item::FILE_TRANSFER_TYPE] = 'Filesystem';
+            $harvestedRecord['fileMetadata'][Builder_Item::FILE_INGEST_OPTIONS] = array();
 
-        // Replace each url by a local path.
-        foreach ($harvestedRecord['fileMetadata']['files'] as &$file) {
-            $path = str_replace('http://www.example.com', TEST_FILES_DIR, $file['source']);
-            $file['Filesystem'] = $path;
-            $file['source'] = $path;
-            unset($file['Url']);
+            // Replace each url by a local path.
+            foreach ($harvestedRecord['fileMetadata']['files'] as &$file) {
+                $path = str_replace('http://www.example.com', TEST_FILES_DIR, $file['source']);
+                $file['Filesystem'] = $path;
+                $file['source'] = $path;
+                unset($file['Url']);
+            }
         }
 
         return $harvestedRecord;
